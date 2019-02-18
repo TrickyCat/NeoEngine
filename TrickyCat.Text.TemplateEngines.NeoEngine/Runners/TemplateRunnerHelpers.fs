c43e969b03aa17326e50e28416f5f249135b4f0b
Dmytro Doroshenko <tricky.cat.code@gmail.com>
2019-02-18 17:27:09 +0200

    Make globalFormatDate lazy

--- TrickyCat.Text.TemplateEngines.NeoEngine/Runners/TemplateRunnerHelpers.fs
+++ TrickyCat.Text.TemplateEngines.NeoEngine/Runners/TemplateRunnerHelpers.fs
@@ -5,14 +5,15 @@ open System.IO
 
 module TemplateRunnerHelpers =
 
-    let globalFormatDate () =
+    let private globalFormatDate = lazy (
         let assembly = Assembly.GetExecutingAssembly()
         let resourceName = "TrickyCat.Text.TemplateEngines.NeoEngine.JS.Global_Scope.formatDate.js"
         use stream = assembly.GetManifestResourceStream(resourceName)
         use reader = new StreamReader(stream)
         let result = reader.ReadToEnd()
         result
+        )
 
     let defaultGlobals = seq {
-        yield globalFormatDate()
+        yield globalFormatDate.Value
         }
