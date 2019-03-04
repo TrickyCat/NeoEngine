99f3ff12c54eacf3dad6d4a8e71a824d8fcb9a02
Dmytro Doroshenko <tricky.cat.code@gmail.com>
2019-03-04 18:52:24 +0200

    Output engine execution duration to console in CliRunner

--- NeoEngine.CliRunner/Program.fs
+++ NeoEngine.CliRunner/Program.fs
@@ -5,45 +5,65 @@ open TrickyCat.Text.TemplateEngines.NeoEngine.ResultCommon
 open IoHelpers
 open TrickyCat.Text.TemplateEngines.NeoEngine.Services
 open System
+open System.Diagnostics
 
 module Main = 
 
-    let renderSvc = TemplateService() :> ITemplateService
+    let private timed f a b c d =
+        let watch = Stopwatch()
+        watch.Start()
+        let result = f a b c d
+        watch.Stop()
+        {| result = result; duration = watch.Elapsed |}
 
-    let writeToConsole suppressOutput content =
+
+    let private logDuration duration =
+        let color = Console.ForegroundColor
+        Console.ForegroundColor <- ConsoleColor.Cyan
+        printfn "Execution duration: %A" duration
+        Console.ForegroundColor <- color
+
+
+    let private renderSvc = TemplateService() :> ITemplateService
+
+    let private writeToConsole suppressOutput content =
         if not suppressOutput then
             printfn "Render result:%s%s%s" nl nl content
 
 
-    let runOnFiles (options: CliOptions): Result<unit, string> = result {
+    let private runOnFiles (options: CliOptions): Result<unit, string> = result {
         let! template = getTemplateFromFile options.TemplateFilePath
         let! context = getContextDataFromFile options.ContextDataFilePath
         let! includes = getIncludesFromFolder options.IncludesFolderPath
         let! globals = getGlobalsFromFolder options.GlobalsFolderPath
 
-        let! renderResult = renderSvc.RenderTemplateString globals includes template context
+        let timedResult = timed renderSvc.RenderTemplateString globals includes template context
+        logDuration timedResult.duration
+        let! result = timedResult.result
 
-        writeToConsole options.SuppressOutputToConsole renderResult
-        do! writeAllTextToFile options.RenderedOutputFilePath renderResult
+        writeToConsole options.SuppressOutputToConsole result
+        do! writeAllTextToFile options.RenderedOutputFilePath result
         }
 
-    let printError error =
+
+    let private printError error =
         let color = Console.ForegroundColor
         Console.ForegroundColor <- ConsoleColor.Red
         printfn "Error%s%s" nl error
         Console.ForegroundColor <- color
 
 
-    let outputErrors (result: Result<_, string>) =
+    let private outputErrors (result: Result<_, string>) =
         result |> Result.mapError printError |> ignore
 
 
-    let runCli argv =
+    let private runCli argv =
         let parseResult = Parser.Default.ParseArguments<CliOptions> argv
         ParserResultExtensions.WithParsed(parseResult, runOnFiles >> outputErrors) |> ignore
 
 
     [<EntryPoint>]
-    let main argv = 
+    let main argv =
+        printfn ""
         runCli argv
         0
