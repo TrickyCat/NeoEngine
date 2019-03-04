5d968ce4c7c0a01d1a1143c28407655e5515cd76
Dmytro Doroshenko <tricky.cat.code@gmail.com>
2019-03-04 18:51:41 +0200

    Add comment

--- TrickyCat.Text.TemplateEngines.NeoEngine/Interpreters/InterpreterBase.fs
+++ TrickyCat.Text.TemplateEngines.NeoEngine/Interpreters/InterpreterBase.fs
@@ -9,6 +9,10 @@ module InterpreterBase =
     /// Abstracts the interpreter being used for template's rendering.
     /// Inherits <see cref="System.IDisposable" /> thus expecting that each interpreter implementation's instance 
     /// should be used for rendering of single template and then explicitly disposed.
+    ///
+    /// Its methods execute the provided strings with JavaScript and return
+    /// either a value in case of success or a string with an error message in case of failure.
+    ///
     /// <see cref="System.IDisposable.Dispose" /> method implementation should clean up JS session values.
     /// </summary>
     type IInterpreter =
