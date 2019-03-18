7d86533b3f6d2d7dbb6bc4354093e746043efa5b
Dmytro Doroshenko <tricky.cat.code@gmail.com>
2019-03-18 16:34:37 +0200

    Rename example project

--- NeoEngine.ConsumerExample.EventsQueueListenerAndEmailSender/AssemblyInfo.fs
+++ NeoEngine.ConsumerExample.EventsQueueListenerAndEmailSender/AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("NeoEngine.ConsumerExample.FSharp")>]
+[<assembly: AssemblyTitle("NeoEngine.ConsumerExample.EventsQueueListenerAndEmailSender")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("NeoEngine.ConsumerExample.FSharp")>]
+[<assembly: AssemblyProduct("NeoEngine.ConsumerExample.EventsQueueListenerAndEmailSender")>]
 [<assembly: AssemblyCopyright("Copyright © Dmytro Doroshenko 2019")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
